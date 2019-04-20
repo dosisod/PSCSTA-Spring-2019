242be81192246f740fd51a6d8718d4eb32d0e4f2
dosisod <39638017+dosisod@users.noreply.github.com>
2019-04-20 14:04:35 -0700

    default loader code

--- test/load.py
+++ test/load.py
@@ -1,5 +1,6 @@
 from data_parser import parse
 
 if __name__=="__main__":
+    #x=parse("/media/asdazey/PSCSTA/Judge/",True)
     x=parse("/home/asdazey/Desktop/PSCSTA/aiden/repeat/repeat.in",True)
     print(x)
